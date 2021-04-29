ae9df8c05288a6865156b5e6ab397d302b003dc5
YBenPan <ybenp8104@outlook.com>
2021-04-28 23:26:36 -0400

    fixed lambda code

--- lambda_code.py
+++ lambda_code.py
@@ -10,6 +10,7 @@ class TrieNode:
 
     def __init__(self, char):
         self.char = char
+        self.word_end = False
         self.children = {}
 
 
@@ -25,30 +26,35 @@ class Trie:
             if char in node.children:  # found
                 node = node.children[char]
 
-            else:
+            else:   # not found
                 new_node = TrieNode(char)
-                node.children[char] = new_node
+                node.children[char] = new_node      # Points to new node
                 node = new_node
+        node.word_end = True
 
     def remove(self, node, keyword):
 
-        if not node.children:  # We have reached the end of the word
-            return 1
-        if keyword == '': 
-            return 2
+        if keyword == '':
+            if not node.word_end:
+                return 2
+            elif node.children:
+                node.word_end = False
+                return 0
+            else:
+                return 1
         char = keyword[0]
         keyword = keyword[1:]
         if char not in node.children:  # We did not find the word
-            return 2    # Error Code
+            return 2  # Error Code
 
         # the function returns True if the child is to be removed
         res = self.remove(node.children[char], keyword)
-        if res == 2:    # Return error code
+        if res == 2:  # Return error code
             return 2
         if res == 0:  # We did not delete the child node, so we will not delete current node either
             return 0
         del node.children[char]
-        if not node.children:  # After deletion, the current node is a leaf. Mark it for removal
+        if (not node.children) and (not node.word_end):  # After deletion, the current node is a leaf. Mark it for removal
             return 1
         return 0
 
@@ -60,14 +66,14 @@ class Trie:
                 node = node.children[char]
             else:
                 return False
-            if (i == len(keyword) - 1) and node.children:   # If we iterated through keyword and there are still children, then our keyword is a prefix. Invalid
+            if (i == len(keyword) - 1) and not node.word_end:  # If we iterated through keyword and there are still children, then our keyword is a prefix. Invalid
                 return False
         return True
 
     def autocomplete_helper(self, cur_string, node):  # DFS starting at prefix node
         cur_string += node.char
         ans = []
-        if not node.children:
+        if node.word_end:
             ans.append(cur_string)
         for child in node.children.values():
             ans += self.autocomplete_helper(cur_string, child)
@@ -94,7 +100,6 @@ class Trie:
             ans += self.print_trie(child_node, keyword_prefix + child, next_print_prefix)
         return ans
 
-
 def decision(event_type, keyword, trie_obj):
     # print(event_type, keyword)
     json_return = ''
@@ -102,11 +107,14 @@ def decision(event_type, keyword, trie_obj):
         trie_obj.add(keyword)
         json_return = 'Addition Successful!'
     elif event_type == 'delete':
-        result = trie_obj.remove(trie_obj.root, keyword)
-        if result == 2:
-            json_return  = 'ERROR: Keyword not found!'
-        else:
-            json_return  = 'Deletion Successful!'
+        if not trie_obj.root.children:      # Trie is empty
+            json_return = 'ERROR: Keyword not found!'
+        else: 
+            result = trie_obj.remove(trie_obj.root, keyword)
+            if result == 2:   # Can't find keyword 
+                json_return  = 'ERROR: Keyword not found!'
+            else:
+                json_return  = 'Deletion Successful!'
     elif event_type == 'search':
         result = trie_obj.search(keyword)
         if result:
@@ -137,14 +145,14 @@ def lambda_handler(event, context):
         s3_resource.Object(bucket, file_name).load()
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == '404':
-            print('INITIALIZING TRIE')
+            print('Initialization Successful!')
             trie_obj = Trie()
     else:
         download_file = s3client.get_object(Bucket=bucket, Key=file_name)
         content = download_file['Body']
         trie_obj = pickle.loads(content.read())
         print('DOWNLOAD SUCCESSFUL')
-
+    
     json_return = decision(event_type, keyword, trie_obj)
 
     # Upload
